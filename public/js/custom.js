8fbac7a8ccb73352ec59eda9ee23d85f304ff8a9
pairin1984 <pairinlek@gmail.com>
2021-06-11 11:11:17 +0700

    update css

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,81 +1,62 @@
-(function () {
-  'use strict';
-
+(function(){
   $(window).scroll(function () {
-    var top = $(document).scrollTop();
-    if (top > 50) {
-      $('#home > .navbar').removeClass('navbar-transparent');
-    } else {
-      $('#home > .navbar').addClass('navbar-transparent');
-    }
-  })
-
-  $('a[href="#"]').click(function (event) {
-    event.preventDefault();
-  })
-
-  $('.bs-component').each(function () {
-    var $component = $(this);
-    var $button = $('<button class="source-button btn btn-primary btn-xs" role="button" tabindex="0">&lt; &gt;</button>');
-    $component.append($button);
-
-    if ($component.find('[data-bs-toggle="tooltip"]').length > 0) {
-      $component.attr('data-html', $component.html());
-    }
+      var top = $(document).scrollTop();
+      $('.splash').css({
+        'background-position': '0px -'+(top/3).toFixed(2)+'px'
+      });
+      if(top > 50)
+        $('#home > .navbar').removeClass('navbar-transparent');
+      else
+        $('#home > .navbar').addClass('navbar-transparent');
   });
 
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
 
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-
-  var sourceModalElem = document.getElementById('source-modal');
-  if (sourceModalElem) {
-    var sourceModal = new bootstrap.Modal(document.getElementById('source-modal'));
-  }
-
-  $('body').on('click', '.source-button', function (event) {
-    event.preventDefault();
-
-    var component = $(this).parent();
-    var html = component.attr('data-html') ? component.attr('data-html') : component.html();
-
+  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+    var html = $(this).parent().html();
     html = cleanSource(html);
-    html = Prism.highlight(html, Prism.languages.html, 'html');
-    $('#source-modal code').html(html);
-    sourceModal.show();
-  })
+    $("#source-modal pre").text(html);
+    $("#source-modal").modal();
+  });
+
+  $('.bs-component [data-toggle="popover"]').popover();
+  $('.bs-component [data-toggle="tooltip"]').tooltip();
+
+  $(".bs-component").hover(function(){
+    $(this).append($button);
+    $button.show();
+  }, function(){
+    $button.hide();
+  });
 
   function cleanSource(html) {
-    html = html.replace(/×/g, '&times;')
-               .replace(/«/g, '&laquo;')
-               .replace(/»/g, '&raquo;')
-               .replace(/←/g, '&larr;')
-               .replace(/→/g, '&rarr;')
+    html = html.replace(/×/g, "&times;")
+               .replace(/«/g, "&laquo;")
+               .replace(/»/g, "&raquo;")
+               .replace(/←/g, "&larr;")
+               .replace(/→/g, "&rarr;");
 
     var lines = html.split(/\n/);
 
     lines.shift();
     lines.splice(-1, 1);
 
-    var indentSize = lines[0].length - lines[0].trim().length;
-    var re = new RegExp(' {' + indentSize + '}');
+    var indentSize = lines[0].length - lines[0].trim().length,
+        re = new RegExp(" {" + indentSize + "}");
 
-    lines = lines.map(function (line) {
+    lines = lines.map(function(line){
       if (line.match(re)) {
-        line = line.slice(Math.max(0, indentSize));
+        line = line.substring(indentSize);
       }
 
       return line;
     });
 
-    lines = lines.join('\n');
+    lines = lines.join("\n");
 
     return lines;
   }
+
 })();
