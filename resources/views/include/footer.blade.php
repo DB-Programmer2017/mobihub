ca80a2c78547d6214da41116fcdd99480fd3706e
pairin1984 <pairinlek@gmail.com>
2021-06-11 11:21:36 +0700

    update footer

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,50 @@
+<section class="footer">
+    <img src="/image/M.png" class="img-responsive image">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <h4>ลงทะเบียนรับข้อมูลข่าวสาร</h4>
+                <div class="form-group">
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="กรอกอีเมลของคุณที่นี่">
+                      <span class="input-group-btn">
+                        <button class="btn btn-primary" type="button">ติดตาม</button>
+                      </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3 about">
+                <h4>Mobihub</h4>
+                <li>เกี่ยวกับเรา</li>
+                <li>ข่าวสาร</li>
+                <li>สินค้า</li>
+                <li>บริการ</li>
+                <li>ติดต่อเรา</li>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <h4>เกี่ยวกับ Mobihub</h4>
+                <p>
+                    ผู้จัดจำหน่ายแบบ Business-to-Business (B2B) ที่ได้รับอนุญาตและบริการที่มีการจัดการซึ่งจำหน่ายสนับสนุนซ่อมแซมผลิตภัณฑ์
+                    และโซลูชันที่ครอบคลุมบน Samsung Android, Android, 
+                    iOS, Windows OS และสินค้า Gadget อุปกรณ์เสริม SmartPhone, 
+                    Tablet สินค้าไลฟ์สไตล์ และสินค้าเทคโนโลยีแบรนด์ดังระดับโลก 
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <h4>ติดต่อเรา</h4>
+                <li>
+                    1448/15 Crystal Design Center (CDC), L2, 2FL., 
+                    Room 202, 204, 206, 208, Soi Ladprao 87 (Chandra Suk), 
+                    Praditmanuthum Rd. , Klongjan, Bangkapi, Bangkok 10240 THAILAND. 
+                </li>
+                <li><i class="far fa-envelope"></i> sales@mobihub.co.th</li>
+                <li><i class="fas fa-phone-volume"></i> 099-287-4710 ถึง 4711</li>
+            </div>
+
+        </div>
+    </div>
+    <div class="copy-right">Copyright © Mobihub all rights reserved. </div>
+</section>
