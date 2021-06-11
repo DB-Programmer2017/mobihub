0a05ae222a0c5efabb17d7ee22de52cc5bd8f75e
pairin1984 <pairinlek@gmail.com>
2021-06-11 10:43:37 +0700

    update navbar

---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,76 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="navbar-top">
+
+    <ul class="navbar-top-left">
+      <li class="social"><i class="fab fa-facebook-f"></i></li>
+      <li class="social"><i class="fab fa-instagram"></i></li>
+      <li class="social"><i class="fab fa-linkedin-in"></i></li>
+      <li class="social"><i class="fab fa-line"></i></li>
+      <li class="social"><i class="fab fa-youtube"></i></li>
+    </ul>
+
+    <ul class="navbar-center">
+      <li><img src="/image/logo_web.png" class="img-brand"></li>
+    </ul>
+
+    <ul class="navbar-top-right">
+      <li><i class="far fa-envelope"></i> sales@mobihub.co.th</li>
+      <li><i class="fas fa-phone-volume"></i> 099-287-4710 ถึง 4711</li>
+    </ul>
+  </div>
+
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        {{-- <a class="navbar-brand" href="#">Mobihub</a> --}}
+      </div>
+  
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="#">หน้าแรก</a></li>
+          <li><a href="#">เกี่ยวกับเรา</a></li>
+
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">สินค้า <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+              <li class="divider"></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+
+          <li><a href="#">บริการ</a></li>
+          <li><a href="#">ข่าวสาร</a></li>
+          <li><a href="#">ติดต่อเรา</a></li>
+          
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <button type="button" class="btn btn-primary btn-quotation">ใบเสนอราคา</button>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Thai <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+              <li class="divider"></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/layout/home-2.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" itemtype="https://schema.org/WebPage">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" >
+    <link href="https://fonts.googleapis.com/css?family=Prompt|Poppins|Questrial|Merienda|Caveat+Brush|Oswald|Anton|Mitr|Abril+Fatface" rel="stylesheet">
+    
+    <link rel="stylesheet" href="/css/layout/style.css">
+
+    @yield('link')
+
+</head>
+<body>
+    <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "106561104903497");
+      chatbox.setAttribute("attribution", "biz_inbox");
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v10.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <div>
+        @include('/include/navbar')
+    </div>
+    <main>
+        <style>
+            /* main{
+                margin-top: 125px;
+            }
+            @media  (max-width: 1023px){
+                main{
+                margin-top: 73px;
+                }
+            } */
+        </style>
+        @yield('content')
+    </main>
+        @include('/include/footer')
+</body>
+
+@yield('script')
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/custom.js"></script>
