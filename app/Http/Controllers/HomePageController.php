19d1ffb1658ac68ec6c0874c644ad0831939e81f
charnmongkol <79741950+charnmongkol@users.noreply.github.com>
2021-06-23 09:56:03 +0700

    update 23-6

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SlideAllModel;
+
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+    public function slideIndex() {
+        
+    }
+}
